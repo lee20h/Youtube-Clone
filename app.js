62b8219815d58dcaea9ca8c133eab087fa09a24d
Young Hun <lee0h.0417@gmail.com>
2020-12-26 19:13:18 +0900

    Add: View init

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ import globalRouter from "./routers/globalRotuer";
 
 const app = express();
 
+app.set("view engine", "pug");
 app.use(cookieParser())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
