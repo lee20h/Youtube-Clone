0671f1107ea6425d32846b789304a26d17e8546c
Young Hun <lee0h.0417@gmail.com>
2020-12-26 21:23:32 +0900

    Modify: View

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 import express from "express";
-import logger from "morgan";
-import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import globalRouter from "./routers/globalRotuer";
+import helmet from "helmet";
+import { localsMiddleware } from "./middlewares";
+import morgan from "morgan";
 import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
-import globalRouter from "./routers/globalRotuer";
 
 const app = express();
 
+app.use(helmet());
 app.set("view engine", "pug");
 app.use(cookieParser())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
-app.use(logger("dev"));
+app.use(morgan("dev"));
+app.use(localsMiddleware);
 
 app.use("/", globalRouter);
 app.use(routes.users, userRouter);
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,3 @@
-export const join = (req, res) => res.render("join");
-export const login = (req, res) => res.render("login");
-export const logout = (req, res) => res.render("logout");
+export const join = (req, res) => res.render("join", { pageTitle: "Join" });
+export const login = (req, res) => res.render("login", { pageTitle: "Login" });
+export const logout = (req, res) => res.render("logout", { pageTitle: "Logout" });
